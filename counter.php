0592bdbfb4ba905429a10ba52e2eec4861909c61
mateusz <mateusz@tadek>
2021-09-17 20:52:40 +0200

    Gotowy licznik

--- counter.php
+++ counter.php
@@ -1,25 +1,36 @@
 <?php 
-$counterFilePath = "\files\counterFile.txt";
+$counterFilePath = "files/counterFile.txt";
 function counter($counterFilePath)
 {
     if(!is_file($counterFilePath))
     {
-        file_put_contents($counterFilePath, "1");
-        return "1";
+        if(is_dir("files"))
+        {
+            $first = (int)"1";
+            file_put_contents($counterFilePath, $first);
+            return "1";
+        }
+        else
+        {
+            mkdir("files");
+        }
     }
     else if(is_file($counterFilePath))
     {
         $file = fopen($counterFilePath, "r+");
+        
         if(flock($file, LOCK_EX))
         {
-            $state = (int)fread($file, filesize($file));
+            $state = fread($file, filesize($counterFilePath));
             $state ++;
             fseek($file, 0);
             fwrite($file, $state);
             flock($file, LOCK_UN);
         }
+        fclose($file);
+        return $state;
     }
 }
 
-counter($counterFilePath);
+echo counter($counterFilePath);
 ?>
