0288154b42dc46575fd0c254e9c66f7cbb4ffa7c
mateusz <mateusz@tadek>
2021-09-17 22:10:45 +0200

    html start building

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+	<meta charset="utf-8" />
+	<title>(T)SoftSell - Testowe środowisko</title>
+	<author content="Mateusz P. 'TadekP'"/>
 </head>
 
 <body>
-
+<?php 
+echo "Hello World!";
+?>
 </body>
 </html>
