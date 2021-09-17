3bf895543429f1f5352f609863121e5ddd3c1d7c
mateusz <mateusz@tadek>
2021-09-17 20:03:50 +0200

    Start

--- counter.php
+++ counter.php
@@ -7,15 +7,15 @@ function counter($counterFilePath)
         file_put_contents($counterFilePath, "1");
         return "1";
     }
-    else{
-        echo "GRA7";
-    }
-    /* else if(is_file($counterFilePath))
+    else if(is_file($counterFilePath))
     {
         $file = fopen($counterFilePath, "r+");
-        
-        
-    } */
+        if(flock($file, LOCK_EX))
+        {
+            $ = (int)fread($file, filesize($file));
+            
+        }
+    }
 }
 
 counter($counterFilePath);
